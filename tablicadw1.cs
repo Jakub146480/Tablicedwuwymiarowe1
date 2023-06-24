8ce33b4b71aec25bc4a2c7ac535e14aa00be14cb
Jakub146480 <128227074+Jakub146480@users.noreply.github.com>
2023-06-24 14:07:39 +0200

    Dodaj pliki projektów.

---
+++ tablicadw1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class tablicadw1
+{
+    static void Main(string[] args)
+    {
+        int[,] tablica = new int[10, 10];
+        int counter = 1;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                // Wiersze parzyste - wypełnianie rosnąco
+                for (int j = 0; j < 10; j++)
+                {
+                    tablica[i, j] = counter;
+                    counter++;
+                }
+            }
+            else
+            {
+                // Wiersze nieparzyste - wypełnianie malejąco
+                for (int j = 9; j >= 0; j--)
+                {
+                    tablica[i, j] = counter;
+                    counter++;
+                }
+            }
+        }
+
+        // Wyświetlanie tablicy
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
